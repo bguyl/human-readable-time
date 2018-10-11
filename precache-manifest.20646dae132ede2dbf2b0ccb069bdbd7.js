a909ab621f3cbb390d2572f04d9c4e84ebd33c40
Bastien Guyl <bastien@guyl.me>
2018-10-11 19:31:53 +0200

    GitHub page

---
+++ precache-manifest.20646dae132ede2dbf2b0ccb069bdbd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b8092f8a965d554e9de1",
+    "url": "/js/chunk-vendors.3d6db11a.js"
+  },
+  {
+    "revision": "4a83dbc6cf3b348b1271",
+    "url": "/js/app.a494dee6.js"
+  },
+  {
+    "revision": "c9dd2748fd40612f459bc052c086a763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8092f8a965d554e9de1",
+    "url": "/css/chunk-vendors.c0572a99.css"
+  },
+  {
+    "revision": "4a83dbc6cf3b348b1271",
+    "url": "/css/app.e6a5b39e.css"
+  }
+];
